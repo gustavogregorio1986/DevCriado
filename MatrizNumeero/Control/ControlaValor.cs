cff37df27c786e71a358014868b459535d863ae7
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-03-12 16:38:12 -0300

    Desenvolver MatrizNumero na orinetação a objetos

---
+++ MatrizNumeero/Control/ControlaValor.cs
@@ -0,0 +1,36 @@
+﻿using MatrizNumeero.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizNumeero.Control
+{
+    public class ControlaValor
+    {
+        public void ImpressaoMatriz(Valor valor)
+        {
+            for (int i = 0; i <= valor.Numero; i++)
+            {
+                for (int j = 0; j <= valor.Numero; j++)
+                {
+                    Console.Write($"{i * j,6}");
+                }
+
+            }
+        }
+
+        public void ImpressaoMatrizSemMultiplicacao(Valor valor)
+        {
+            for (int i = 0; i <= valor.Numero; i++)
+            {
+                for (int j = 0; j <= valor.Numero; j++)
+                {
+                    Console.Write($"{i},{j,6}");
+                }
+
+            }
+        }
+    }
+}
---
+++ MatrizNumeero/Program.cs
@@ -0,0 +1,11 @@
+﻿using MatrizNumeero.Control;
+using MatrizNumeero.Entidade;
+
+Valor valor = new Valor();
+valor.Numero = 21;
+
+Console.WriteLine($"Numero: {valor.Numero}");
+
+ControlaValor controlaValor = new ControlaValor();
+controlaValor.ImpressaoMatriz(valor);
+controlaValor.ImpressaoMatrizSemMultiplicacao(valor);
