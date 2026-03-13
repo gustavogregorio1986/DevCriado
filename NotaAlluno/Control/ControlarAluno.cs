24e901a2121c1fee4bd4e02f6d05e1353d0d2152
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-03-13 11:01:04 -0300

    Desenvolvimento de orientaçõa  a objetos com sobrecarga e com alunos e os cauclos dos alunos.

---
+++ NotaAlluno/Control/ControlarAluno.cs
@@ -0,0 +1,39 @@
+﻿using NotaAlluno.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaAlluno.Control
+{
+    public class ControlarAluno
+    {
+        public double MediaAluno(Aluno aluno)
+        {
+            return (aluno.Nota1 + aluno.Nota2) / 2;
+        }
+
+        public string SituacaoAluno(Aluno aluno)
+        {
+            double media = MediaAluno(aluno);
+
+            if (media >= 6.0)
+            {
+                return "Aprovado";
+            }
+            else if (media >= 5.0)
+            {
+                double mediaRecuperacao = (media + aluno.Recuperacao) / 2;
+                if (mediaRecuperacao >= 6.0)
+                    return "Aprovado em Recuperação";
+                else
+                    return "Reprovado após Recuperação";
+            }
+            else
+            {
+                return "Reprovado";
+            }
+        }
+    }
+}
---
+++ NotaAlluno/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NotaAlluno.Control;
+using NotaAlluno.Entidade;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno marina = new Aluno
+        {
+            NomeAluno = "Marina",
+            EmailAluno = "marina@gmail.com",
+            Nota1 = 6.9,
+            Nota2 = 7.9
+        };
+
+        Aluno luiz = new Aluno
+        {
+            NomeAluno = "Luiz",
+            EmailAluno = "luiz@gmail.com",
+            Nota1 = 2.9,
+            Nota2 = 3.9,
+            Recuperacao = 6.0 // exemplo de nota de recuperação
+        };
+
+        Aluno leni = new Aluno
+        {
+            NomeAluno = "Leni",
+            EmailAluno = "leni@gmail.com",
+            Nota1 = 2.9,
+            Nota2 = 1.6
+        };
+
+        ControlarAluno controlar = new ControlarAluno();
+
+        // Marina
+        Console.WriteLine($"Nome do Aluno: {marina.NomeAluno}");
+        Console.WriteLine($"Email do Aluno: {marina.EmailAluno}");
+        Console.WriteLine($"Nota 1: {marina.Nota1}");
+        Console.WriteLine($"Nota 2: {marina.Nota2}");
+        Console.WriteLine($"Média: {controlar.MediaAluno(marina)}");
+        Console.WriteLine($"Situação: {controlar.SituacaoAluno(marina)}");
+        Console.WriteLine();
+
+        // Luiz
+        Console.WriteLine($"Nome do Aluno: {luiz.NomeAluno}");
+        Console.WriteLine($"Email do Aluno: {luiz.EmailAluno}");
+        Console.WriteLine($"Nota 1: {luiz.Nota1}");
+        Console.WriteLine($"Nota 2: {luiz.Nota2}");
+        Console.WriteLine($"Média: {controlar.MediaAluno(luiz)}");
+        Console.WriteLine($"Situação: {controlar.SituacaoAluno(luiz)}");
+        Console.WriteLine();
+
+        // Luiz
+        Console.WriteLine($"Nome do Aluno: {leni.NomeAluno}");
+        Console.WriteLine($"Email do Aluno: {leni.EmailAluno}");
+        Console.WriteLine($"Nota 1: {leni.Nota1}");
+        Console.WriteLine($"Nota 2: {leni.Nota2}");
+        Console.WriteLine($"Média: {controlar.MediaAluno(leni)}");
+        Console.WriteLine($"Situação: {controlar.SituacaoAluno(leni)}");
+
+        Console.ReadLine();
+    }
+}
