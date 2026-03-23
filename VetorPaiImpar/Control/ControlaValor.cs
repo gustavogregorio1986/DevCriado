2815334c71e1bb04ad0a1255d60d5f6b9061e7b4
Miguel Marquiori <miguelmarkiori@outlook.com>
2026-03-23 15:29:57 -0300

    Desenvolvendo vetor no C# na programação

---
+++ VetorPaiImpar/Control/ControlaValor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetorPaiImpar.Entidade;
+
+namespace VetorPaiImpar.Control
+{
+    public class ControlaValor
+    {
+        public void VetorParImpar(Valor valor)
+        {
+            for(int i = 0; i <= valor.Numero; i++)
+            {
+                if (valor.Numero % 2 == 0)
+                {
+                    Console.WriteLine("Numero par" + valor.Numero);
+                }
+                else
+                {
+                    Console.WriteLine("Numero impar" + valor.Numero);
+                }
+        }
+    }
+}
